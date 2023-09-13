ae18b44e77f50bf7d479f1f388a3b05ce19db170
TommiKivikangas <144022435+TommiKivikangas@users.noreply.github.com>
2023-09-13 11:06:20 +0300

    Add files via upload

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 8f;
+    public Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Moving the player
+        Vector3 movement = new Vector3(movementX, rb.velocity.y, movementY);
+        rb.velocity = movement * moveSpeed;
+    }
+
+    // OnMove is called when Move input is used.
+    public void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+}
